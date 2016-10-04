389bd7d299898b50a38123f4116473161e1156e3
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-10-04 07:42:27 +0600

    Q7

--- app/controllers/page1.controller.js
+++ app/controllers/page1.controller.js
@@ -8,7 +8,25 @@
         // Q2: common service
         $scope.commonService = CommonService;
 
+        // Q5: Phone model value
+        $scope.phoneNumber;
+
         // Q6: Currency model value
         $scope.currencyAmount;
+
+        // Q7: input values
+
+        $scope.inputValues = {
+            input1: 100,
+            input2: 100,
+            input3: 100,
+            sum: ""
+        };
+        $scope.sumInputs = function(){
+            var inp = $scope.inputValues;
+            inp.sum = inp.input1 + inp.input2 + inp.input3;
+            console.log(inp.sum);
+        };
+        $scope.sumInputs();
     }
 }());
